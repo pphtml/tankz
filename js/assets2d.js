b3d23c88752c6adda5719d94df7ec42db0720845
petr <petr.pravda@centrum.cz>
2013-04-03 22:32:18 +0200

    r30

--- js/assets2d.js
+++ js/assets2d.js
@@ -20,26 +20,62 @@ BaseAsset.prototype.onMouseDown = function(e, pixelCoords, unit, dctx) {
     return inside;
 };
 
-BaseAsset.prototype.drawLifeBar = function() {
-    var x = this.drawingX;
-    var y = this.drawingY - 2*game.lifeBarHeight;
-    game.foregroundContext.fillStyle = (this.lifeCode == "healthy")?game.healthBarHealthyFillColor:game.healthBarDamagedFillColor;          
-    game.foregroundContext.fillRect(x,y,this.pixelWidth*this.life/this.hitPoints,game.lifeBarHeight)
-    game.foregroundContext.strokeStyle = game.healthBarBorderColor;
-    game.foregroundContext.lineWidth = 1;
-    game.foregroundContext.strokeRect(x,y,this.pixelWidth,game.lifeBarHeight)
+BaseAsset.prototype.computePosRect = function(unit, img) {
+    return { x: parseInt(unit.x + this.scale * img.cx),
+        y: parseInt(unit.y + this.scale * (img.cy + this.yOffset)),
+        w: this.scale * img.w,
+        h: this.scale * img.h
+    };
 };
 
-BaseAsset.prototype.drawSelection = function(dctx, rect){
-//    var x = this.drawingX + this.pixelOffsetX;
-//    var y = this.drawingY + this.pixelOffsetY;
-//    game.foregroundContext.strokeStyle = game.selectionBorderColor;
+BaseAsset.prototype.draw = function(dctx, unit) {
+    var img = this.getSpriteImage(unit);
+    //console.info(img);
+    var pos = this.computePosRect(unit, img);
+    dctx.ctx.drawImage(this.atlas_image, img.x, img.y, img.w, img.h,
+        pos.x, pos.y, pos.w, pos.h);
+    
+    if (unit.selected) {
+        this.drawSelection(dctx, pos);
+    }
+    
+    if (typeof unit.path != 'undefined') {
+        //console.info("xxxxxxxxxxxxxxxx " + dctx.grid);
+        for(var i = 0, count = unit.path.length; i < count; i++) {
+            var node = unit.path[i];
+            //console.info(node.x, node.y);
+            var pixelCoords = dctx.grid.locatePixelCoords(node.x, node.y);
+            var context = dctx.ctx;
+            context.beginPath();
+            context.arc(pixelCoords.x, pixelCoords.y, 2, 0 , 2 * Math.PI, false);
+            context.fillStyle = 'green';
+            context.fill();
+            context.lineWidth = 1;
+            context.strokeStyle = '#003300';
+            context.stroke();
+        }
+    }
+};
+
+BaseAsset.prototype.getIndexedImage = function(spriteName, spriteIndex) {
+    var name = spriteName + (spriteIndex < 10 ? '0' : '') + spriteIndex + '.png';
+    var img = this.atlas_data[name];
+    if (!img) {
+        throw ('Missing sprite ' + name);
+    }
+    return img;
+};
+//BaseAsset.prototype.drawLifeBar = function() {
+//    var x = this.drawingX;
+//    var y = this.drawingY - 2*game.lifeBarHeight;
+//    game.foregroundContext.fillStyle = (this.lifeCode == "healthy")?game.healthBarHealthyFillColor:game.healthBarDamagedFillColor;          
+//    game.foregroundContext.fillRect(x,y,this.pixelWidth*this.life/this.hitPoints,game.lifeBarHeight)
+//    game.foregroundContext.strokeStyle = game.healthBarBorderColor;
 //    game.foregroundContext.lineWidth = 1;
-//    game.foregroundContext.beginPath();
-//    game.foregroundContext.arc(x,y,this.radius,0,Math.PI*2,false);
-//    game.foregroundContext.fillStyle = game.selectionFillColor;
-//    game.foregroundContext.fill();
-//    game.foregroundContext.stroke();
+//    game.foregroundContext.strokeRect(x,y,this.pixelWidth,game.lifeBarHeight)
+//};
+
+BaseAsset.prototype.drawSelection = function(dctx, rect){
     var context = dctx.ctx;
     context.beginPath();
 //    var x = 0.5 + parseInt(rect.x);
@@ -61,50 +97,7 @@ var TankAsset = function() {
     this.scale = 0.6;
     
     this.getSpriteImage = function(unit) {
-        var spriteIndex = unit.spriteIndex(); 
-        var name = 'tank' + (spriteIndex < 10 ? '0' : '') + spriteIndex + '.png';
-        var img = this.atlas_data[name];
-        if (!img) {
-            throw ('Missing sprite ' + name);
-        }
-        return img;
-    };
-
-    this.computePosRect = function(unit, img) {
-        return { x: parseInt(unit.x + this.scale * img.cx),
-            y: parseInt(unit.y + this.scale * (img.cy + this.yOffset)),
-            w: this.scale * img.w,
-            h: this.scale * img.h
-        };
-    };
-
-    this.draw = function(dctx, unit) {
-        var img = this.getSpriteImage(unit);
-        //console.info(img);
-        var pos = this.computePosRect(unit, img);
-        dctx.ctx.drawImage(this.atlas_image, img.x, img.y, img.w, img.h,
-            pos.x, pos.y, pos.w, pos.h);
-        
-        if (unit.selected) {
-            this.drawSelection(dctx, pos);
-        }
-        
-        if (typeof unit.path != 'undefined') {
-            //console.info("xxxxxxxxxxxxxxxx " + dctx.grid);
-            for(var i = 0, count = unit.path.length; i < count; i++) {
-                var node = unit.path[i];
-                //console.info(node.x, node.y);
-                var pixelCoords = dctx.grid.locatePixelCoords(node.x, node.y);
-                var context = dctx.ctx;
-                context.beginPath();
-                context.arc(pixelCoords.x, pixelCoords.y, 2, 0 , 2 * Math.PI, false);
-                context.fillStyle = 'green';
-                context.fill();
-                context.lineWidth = 1;
-                context.strokeStyle = '#003300';
-                context.stroke();
-            }
-        }
+        return this.getIndexedImage('tank', unit.spriteIndex());
     };
 };
 TankAsset.prototype = new BaseAsset();
@@ -126,6 +119,110 @@ GenericUnit.prototype.onMouseDown = function(e, pixelCoords, dctx) {
     return this.asset.onMouseDown(e, pixelCoords, this, dctx);
 };
 
+GenericUnit.prototype.tick = function(timeDelta, dctx) { // todo premistit do generic unit
+    var moved = false;
+    
+    while (timeDelta > 0.0) {
+        if (typeof this.movegrid == 'undefined' && typeof this.path != 'undefined' && this.path.length > 0) {
+            var node = this.path.splice(0, 1)[0];
+            var pixelCoords = dctx.grid.locatePixelCoords(node.x, node.y);
+            // todo pocitat jenom pri prechodu na novou bunku
+            var angle = dctx.angle.compute(node.x - this.gridX, this.gridY - node.y);
+            //var angle = dctx.angle.compute(pixelCoords.x - this.x, this.y - pixelCoords.y);
+            //console.info("changing rotation to " + angle);
+            this.rotation = angle;
+
+            // todo kontrolovat obsazenost bunky
+//            this.x = pixelCoords.x;
+//            this.y = pixelCoords.y;
+//            this.gridX = node.x;
+//            this.gridY = node.y;
+            this.movegrid = {
+                addingX: pixelCoords.x > this.x ? 1 : pixelCoords.x < this.x ? -1 : 0,
+                addingY: pixelCoords.y > this.y ? 1 : pixelCoords.y < this.y ? -1 : 0,
+//                dx: deltas.x,
+//                dy: deltas.y,
+//                reached: function(x, y) { return (this.addingX > 0 && x >= this.targetX) ||
+//                    (this.addingX < 0 && x <= this.targetX) ||
+//                    (this.addingY > 0 && y >= this.targetY) ||
+//                    (this.addingY < 0 && y <= this.targetY); },
+                deltaResiduum: function(x, y, dx, dy, delta) {
+                    var result = 0.0;
+                    if ((this.addingX > 0 && x >= this.targetX) || (this.addingX < 0 && x <= this.targetX)) {
+                        result = (x - this.targetX) / dx * delta;
+                    } else if ((this.addingY > 0 && y >= this.targetY) || (this.addingY < 0 && y <= this.targetY)) {
+                        result = (y - this.targetY) / dy * delta;
+                    }
+                    return result;
+                },
+                targetX: pixelCoords.x,
+                targetY: pixelCoords.y
+            };
+        }
+
+        var innerTickResult = this.innerGridTick(timeDelta, dctx);
+        if (innerTickResult.finished) {
+            delete this.movegrid;
+        }
+
+        moved = moved || innerTickResult.moved;
+        
+        timeDelta = innerTickResult.remainingDelta;
+    }
+    
+    return moved;
+};
+
+GenericUnit.prototype.innerGridTick = function(timeDelta, dctx) {
+    // if (!(this.x % 1 === 0)) {
+    // console.error("X coordinate is a float");
+    // }
+    var moved = false;
+    var finished = false;
+    var remainingDelta = 0.0;
+    if (typeof this.movegrid != 'undefined') {
+        moved = true;
+        var deltas = this.computeMovementDeltas(timeDelta, dctx);
+        // this.x = parseFloat(this.x) + deltas.x;
+        // this.y = parseFloat(this.y) + deltas.y;
+        this.x += deltas.x;
+        this.y += deltas.y;
+        var residuum = this.movegrid.deltaResiduum(this.x, this.y, deltas.x,
+                deltas.y, timeDelta);
+        if (residuum > 0.0) {
+            // console.info("residuum found " + residuum + " from " +
+            // timeDelta);
+            this.x = this.movegrid.targetX;
+            this.y = this.movegrid.targetY;
+            finished = true;
+            remainingDelta = residuum;
+        }
+        var gridCoords = dctx.grid.locateGridCoords(this.x, this.y);
+        this.gridX = gridCoords.x;
+        this.gridY = gridCoords.y;
+    }
+    return {
+        moved : moved,
+        finished : finished,
+        remainingDelta : remainingDelta
+    };
+};
+
+GenericUnit.prototype.computeMovementDeltas = function(timeDelta, dctx) {
+    var speedFactor = this.gridSpeed * timeDelta;
+    var dpx = parseFloat(dctx.grid.pixelsPerTileX) * speedFactor;
+    var dpy = parseFloat(dctx.grid.pixelsPerTileY) * speedFactor;
+    var dirFactors = this.directions[this.rotation];
+    if (dirFactors == null) {
+        console.error("Unknown direction " + this.rotation);
+    }
+    return {x: dpx * dirFactors[0], y: dpy * dirFactors[1]};
+};
+
+GenericUnit.prototype.draw = function(dctx) { 
+    this.asset.draw(dctx, this);
+};
+
 var ZERO = 0.0;
 var DIAG = 1.0 / Math.sqrt(2);
 var FULL = 1.0;
@@ -145,120 +242,11 @@ var Tank = function() {
     this.asset = tank_asset;
     this.gridSpeed = 10.0; // grid/s
     
-    this.draw = function(dctx) { 
-        this.asset.draw(dctx, this);
-    };
-    
     this.spriteIndex = function() {
         var result = Math.floor((this.rotation + 5.625) / 11.25);
         result = result % 32;
         return result;
-    };
-    
-    this.computeMovementDeltas = function(timeDelta, dctx) {
-        var speedFactor = this.gridSpeed * timeDelta;
-        var dpx = parseFloat(dctx.grid.pixelsPerTileX) * speedFactor;
-        var dpy = parseFloat(dctx.grid.pixelsPerTileY) * speedFactor;
-        var dirFactors = this.directions[this.rotation];
-        if (dirFactors == null) {
-            console.error("Unknown direction " + this.rotation);
-        }
-        return {x: dpx * dirFactors[0], y: dpy * dirFactors[1]};
-    };
-
-//    this.computeTimeDelta = function(x, y) {
-//        var speedFactor = this.gridSpeed * timeDelta;
-//        var dpx = parseFloat(dctx.grid.pixelsPerTileX) * speedFactor;
-//        var dpy = parseFloat(dctx.grid.pixelsPerTileY) * speedFactor;
-//        var dirFactors = this.directions[this.rotation];
-//        if (dirFactors == null) {
-//            console.error("Unknown direction " + this.rotation);
-//        }
-//        return {x: dpx * dirFactors[0], y: dpy * dirFactors[1]};
-//    };
-//
-    this.innerGridTick = function(timeDelta, dctx) {
-//        if (!(this.x % 1 === 0)) {
-//            console.error("X coordinate is a float");
-//        }
-        var moved = false;
-        var finished = false;
-        var remainingDelta = 0.0;
-        if (typeof this.movegrid != 'undefined') {
-            moved = true;
-            var deltas = this.computeMovementDeltas(timeDelta, dctx);
-//            this.x = parseFloat(this.x) + deltas.x; 
-//            this.y = parseFloat(this.y) + deltas.y;
-            this.x += deltas.x; 
-            this.y += deltas.y;
-            var residuum = this.movegrid.deltaResiduum(this.x, this.y, deltas.x, deltas.y, timeDelta);
-            if (residuum > 0.0) {
-                //console.info("residuum found " + residuum + " from " + timeDelta);
-                this.x = this.movegrid.targetX;
-                this.y = this.movegrid.targetY;
-                finished = true;
-                remainingDelta = residuum;
-            }
-            var gridCoords = dctx.grid.locateGridCoords(this.x, this.y);
-            this.gridX = gridCoords.x; 
-            this.gridY = gridCoords.y;
-        }
-        return { moved: moved, finished: finished, remainingDelta: remainingDelta };
-    };
-    
-    this.tick = function(timeDelta, dctx) { // todo premistit do generic unit
-        var moved = false;
-        
-        while (timeDelta > 0.0) {
-            if (typeof this.movegrid == 'undefined' && typeof this.path != 'undefined' && this.path.length > 0) {
-                var node = this.path.splice(0, 1)[0];
-                var pixelCoords = dctx.grid.locatePixelCoords(node.x, node.y);
-                // todo pocitat jenom pri prechodu na novou bunku
-                var angle = dctx.angle.compute(node.x - this.gridX, this.gridY - node.y);
-                //var angle = dctx.angle.compute(pixelCoords.x - this.x, this.y - pixelCoords.y);
-                //console.info("changing rotation to " + angle);
-                this.rotation = angle;
-    
-                // todo kontrolovat obsazenost bunky
-    //            this.x = pixelCoords.x;
-    //            this.y = pixelCoords.y;
-    //            this.gridX = node.x;
-    //            this.gridY = node.y;
-                this.movegrid = {
-                    addingX: pixelCoords.x > this.x ? 1 : pixelCoords.x < this.x ? -1 : 0,
-                    addingY: pixelCoords.y > this.y ? 1 : pixelCoords.y < this.y ? -1 : 0,
-    //                dx: deltas.x,
-    //                dy: deltas.y,
-    //                reached: function(x, y) { return (this.addingX > 0 && x >= this.targetX) ||
-    //                    (this.addingX < 0 && x <= this.targetX) ||
-    //                    (this.addingY > 0 && y >= this.targetY) ||
-    //                    (this.addingY < 0 && y <= this.targetY); },
-                    deltaResiduum: function(x, y, dx, dy, delta) {
-                        var result = 0.0;
-                        if ((this.addingX > 0 && x >= this.targetX) || (this.addingX < 0 && x <= this.targetX)) {
-                            result = (x - this.targetX) / dx * delta;
-                        } else if ((this.addingY > 0 && y >= this.targetY) || (this.addingY < 0 && y <= this.targetY)) {
-                            result = (y - this.targetY) / dy * delta;
-                        }
-                        return result;
-                    },
-                    targetX: pixelCoords.x,
-                    targetY: pixelCoords.y
-                };
-            }
-    
-            var innerTickResult = this.innerGridTick(timeDelta, dctx);
-            if (innerTickResult.finished) {
-                delete this.movegrid;
-            }
-    
-            moved = moved || innerTickResult.moved;
-            
-            timeDelta = innerTickResult.remainingDelta;
-        }
-        
-        return moved;
-    };
+    };    
 };
 Tank.prototype = new GenericUnit();
 
